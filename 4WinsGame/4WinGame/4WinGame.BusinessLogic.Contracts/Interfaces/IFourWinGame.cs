0f59869cc92f17cf5eb71687067383287f60e8ec
Jakob Leistner <jakob.leistner@infoteam.de>
2023-02-16 17:03:42 +0100

    Added Exception

---
+++ 4WinsGame/4WinGame/4WinGame.BusinessLogic.Contracts/Interfaces/IFourWinGame.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _4WinGame.BusinessLogic.Contracts.Interfaces
+{
+    public interface IFourWinGame
+    {
+        int ID { get; }
+        EventHandler OnCurrentPlayerChange { get; set; }
+
+        void DoMove(int column);
+    }
+}
