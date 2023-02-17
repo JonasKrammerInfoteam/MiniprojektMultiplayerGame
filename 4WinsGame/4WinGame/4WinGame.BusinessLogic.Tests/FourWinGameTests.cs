8d9064e216b246868c364bc7f40c14c5276f8e7a
Judith Semenyo <judith.semenyo@infoteam.de>
2023-02-17 09:38:00 +0100

    alltest

---
+++ 4WinsGame/4WinGame/4WinGame.BusinessLogic.Tests/FourWinGameTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _4WinGame.BusinessLogic.Tests
+{
+    [TestClass]
+    public class FourWinGameTests 
+    {
+        [TestMethod]
+        public void DoMove_FieldWhereColummIsFull_ColummISFullException()
+        {
+            Assert.AreEqual(true, false);
+        }
+    }
+}
