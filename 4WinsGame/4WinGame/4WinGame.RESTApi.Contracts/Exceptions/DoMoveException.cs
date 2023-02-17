8a95595fa20b371d91be2bb112639c36bcf70593
Simon Roesch <simon.roesch@infoteam.de>
2023-02-17 10:09:56 +0100

    Updated Exceptions

---
+++ 4WinsGame/4WinGame/4WinGame.RESTApi.Contracts/Exceptions/DoMoveException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4WinGame.RESTApi.Contracts.Exceptions
+{
+    public class DoMoveException : Exception
+    {
+
+        public DoMoveException(string message, Exception exception) : base(message, exception)
+        {
+
+        }
+
+        public DoMoveException(Exception exception) : base("Exception while do Move")
+        {
+
+        }
+
+    }
+}
---
+++ 4WinsGame/4WinGame/4WinGame.RESTApi.Contracts/Exceptions/PlayerNotInGameException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4WinGame.RESTApi.Contracts.Exceptions
+{
+    public class PlayerNotInGameException : Exception
+    {
+
+        public PlayerNotInGameException() : base("The chosen player is currently not in a game!")
+        {
+
+        }
+
+        public PlayerNotInGameException(string message) : base(message)
+        {
+
+        }
+
+    }
+}
