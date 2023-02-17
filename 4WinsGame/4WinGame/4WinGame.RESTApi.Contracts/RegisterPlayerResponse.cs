d82743577b9df4f7c5202acc94e23df8a4d120f1
Simon Roesch <simon.roesch@infoteam.de>
2023-02-17 09:18:33 +0100

    Added some RestAPIContracts

---
+++ 4WinsGame/4WinGame/4WinGame.RESTApi.Contracts/RegisterPlayerResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4WinGame.RESTApi.Contracts
+{
+    public class RegisterPlayerResponse
+    {
+
+        public MyPlayer RegisteredPlayer { get; }
+
+    }
+}
