9bca1ddc74250e27e6d4f6cf1b6e8635e594f261
Simon Roesch <simon.roesch@infoteam.de>
2023-03-02 08:59:25 +0100

    + Made some design changes in the Frontend

--- 4WinsGame/4WinGame/AngularWebFrontend/4WinGame/src/app/login/aboutus/aboutus.component.ts
+++ 4WinsGame/4WinGame/AngularWebFrontend/4WinGame/src/app/login/aboutus/aboutus.component.ts
@@ -13,8 +13,8 @@ export class AboutusComponent implements OnInit {
   public ngOnInit() : void {
     this.AddDeveloper(new Developer("Jonas Krammer", ["Tätigkeit als Projektleiter", "Unterstützung bei Fragen & Problemen"], this.pictureLeft));
     this.AddDeveloper(new Developer("Judith Semenyo", ["Schreiben und Durchführen von Unit-Tests", "Aufsetzen eines kleinen Netzwerkes"], this.pictureLeft));
-    this.AddDeveloper(new Developer("Jakob Leistner", ["Entwicklung des RTPHub & der RestAPI", "Umsetzung des Frontends mit Angular"], this.pictureLeft));
-    this.AddDeveloper(new Developer("Simon Rösch", ["Tätigkeit als Projektleiter", "Umsetzung & Weiterentwicklung des Frontends mit Angular"], this.pictureLeft, "../../assets/SimonPicture.jpg"));
+    this.AddDeveloper(new Developer("Jakob Leistner", ["Entwicklung der BusinessLogic", "Umsetzung des Frontends mit Angular"], this.pictureLeft));
+    this.AddDeveloper(new Developer("Simon Rösch", ["Entwicklung des RTPHub & der RestAPI", "Umsetzung & Weiterentwicklung des Frontends mit Angular"], this.pictureLeft, "../../assets/SimonPicture.jpg"));
   }
 
   private AddDeveloper(developer : Developer) : void {
